5cb6fe8ea0e03ec92dab61815cc7e17ee103e311
muntasir <sajeebmuntasir0@gmail.com>
2023-03-11 17:31:19 +0600

    a

--- src/Pages/Auth/SignIn/SignIn.js
+++ src/Pages/Auth/SignIn/SignIn.js
@@ -1,8 +1,102 @@
-import React from 'react'
+import './SignIn.css'
+import { Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
     return (
-        <div>SignIn</div>
+        <>
+            <div className="signinpage">
+
+                <div className="">
+                    <p className="pageTitle">
+                        Bmail Enables Web 3 communication
+                    </p>
+
+                    <p className="pageDes">
+                        BMAIL is a secure and decentralized email service that leverages encryption and blockchain technology to enable Web 3 communication. With bMail, users can send and receive emails directly between their MetaMask wallets, ensuring the privacy and security of their messages.
+                    </p>
+                    <div className="mailIcons">
+                        <div>
+                            <div className="gradIcon">
+
+                            </div>
+                            <p className="iconText">
+                                WebMail
+                            </p>
+                        </div>
+                        <div>
+                            <div className="gradIcon">
+
+                            </div>
+                            <p className="iconText">
+                                Messages
+                            </p>
+                        </div>
+                        <div>
+                            <div className="gradIcon">
+
+                            </div>
+                            <p className="iconText">
+                                Decrypted
+                            </p>
+                        </div>
+                        <div>
+                            <div className="gradIcon">
+
+                            </div>
+                            <p className="iconText">
+                                AI Writer
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="signInForm">
+                    <p className="signUpFormTitle">
+                        Log in to Bmail
+                    </p>
+
+                    <p className="formTexts">
+                        <small>Don't have an account?
+                            <Link to="/user/signup"
+                                className="text-decoration-none"> Create an account here
+                            </Link>
+                        </small>
+                    </p>
+
+                    <Form>
+
+                        <div className="inputGroup">
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="inpField"
+                                placeholder="Username"
+                            />
+                            <p className="bmail">@Bmail</p>
+                        </div>
+
+                        <div className="passFieldslogin">
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                className="inpField"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <Link className="text-decoration-none forgot">Forgot Password</Link>
+
+                        <button type="submit" className="submitbtn">
+                            CONTINUE
+                        </button>
+                    </Form>
+                </div>
+
+
+            </div>
+        </>
     )
 }
 
--- src/Pages/Auth/SignUp/SignUp.js
+++ src/Pages/Auth/SignUp/SignUp.js
@@ -1,3 +1,5 @@
+import { Form, FormCheck } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import "./SignUp.css"
 
 const SignUp = () => {
@@ -5,7 +7,7 @@ const SignUp = () => {
         <>
             <div className="signuppage">
 
-                <div>
+                <div className="leftPartSignup">
                     <p className="pageTitle">
                         Bmail Enables Web 3 communication
                     </p>
@@ -54,6 +56,61 @@ const SignUp = () => {
                     <p className="signUpFormTitle">
                         Create your Bmail Account
                     </p>
+
+                    <p className="formTexts"><small>Already have a Bmail account? <Link to="/user/signin" className="text-decoration-none">Log In here</Link></small></p>
+
+                    <Form>
+
+                        <div className="inputGroup">
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="inpField"
+                                placeholder="Username"
+                            />
+                            <p className="bmail">@Bmail</p>
+                        </div>
+                        <p className="permissionText">
+                            You can use letters, numbers underscores, and dashes.
+                        </p>
+
+
+                        <div className="passFields">
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                className="inpField"
+                                placeholder="Password"
+                            />
+
+                            <input
+                                type="password"
+                                name="repassword"
+                                id="repassword"
+                                className="inpField"
+                                placeholder="Re-type Password"
+                            />
+                        </div>
+
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="I understand that i cannot reset my password and have stored it safely. WHh?" className="checkbox" />
+                        </Form.Group>
+
+                        <div className="checkboxes">
+                            <Form.Check type="checkbox" />
+                            <p className="formTexts m-0">
+                                I agree to <Link className="text-decoration-none">Terms of Service</Link> and <Link className="text-decoration-none">Privacy Policy</Link>
+                            </p>
+                        </div>
+
+
+
+                        <button type="submit" className="submitbtn">
+                            CONTINUE
+                        </button>
+                    </Form>
                 </div>
 
 
